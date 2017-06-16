71c4543760e6da74c42f2136181ea4d70f4f3b7c
r0zbot <>
2017-06-15 22:47:42 -0300

    Loop (98%)perfeito da trilha sonora

---
+++ godot/SoundtrackRepeat.gd
@@ -0,0 +1,16 @@
+extends StreamPlayer
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	
+	pass
+
+
+func _on_Intro_finished():
+	self.set_paused(false)
+	pass # replace with function body
